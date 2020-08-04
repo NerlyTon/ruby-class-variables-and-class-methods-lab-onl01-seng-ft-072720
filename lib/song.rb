fc82ce125798b301af9f8279c3dcc507ea24b979
Nerly Ton <nerlyton@gmail.com>
2020-08-04 17:11:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,10 @@ class Song
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
 end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    genre_count
+end
 end
