68c965d368bb4b342836a37409a623eafe402963
Nerly Ton <nerlyton@gmail.com>
2020-08-04 17:16:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,12 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  @@song_count = 0
+  @@count = 0
   @@genres = []
   @@artists = []
   
   def initialize(name, artist, genre)
-    @@song_count += 1
+    @@count += 1
   
     @name = name
     @artist = artist
@@ -17,7 +17,7 @@ class Song
   end
   
   def self.count 
-    @@song_count
+    @@count
   end
   
   def self.genres 
